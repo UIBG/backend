bb3d650dc286bdc3f80deaad5a706872218ecdc2
Kiel <hezekial7778@gmail.com>
2024-07-06 16:54:32 +0700

    First Commit

---
+++ src/main/java/id/ac/ui/uibg/UibgApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.uibg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UibgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UibgApplication.class, args);
+    }
+
+}
